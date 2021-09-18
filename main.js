066106f5f99f4a7d51ff94acc744eda0c8306e93
wellymaya <welly.maya.s@gmail.com>
2021-09-18 02:52:10 -0300

    adding gameover

--- main.js
+++ main.js
@@ -48,7 +48,8 @@ function iniciarGame() {
 	for(i = 1; i < snake.length; i++){
 		if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
 			clearInterval(jogo);
-			alert("Game over")
+			const gameover = document.querySelector('.gameover')
+			gameover.classList.remove('hidden')
 		}
 		
 	}
